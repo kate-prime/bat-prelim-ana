aa31e736abe69b315dab3c6ae5cebf3d3119c5a2
Kate <kate.allen.89@gmail.com>
2019-09-18 11:18:28 -0400

    Update binfun.m

--- binfun.m
+++ binfun.m
@@ -1,6 +1,6 @@
 
-%modified from AS by KA 2019
 function [data,bins,val]=binfun(data,len,wid,reps)
+%modified from AS by KA 2019
 %bins sorted trials
 %data is a sorted matrix of spike times of of (m * n) where m is spikes in bin and
 %n is number of trials * num reps.
@@ -16,7 +16,7 @@ for j=1:reps:length(data) %deletes spikes that happen after stimulus (between st
 end
   
 for i=1:reps:length(data)
-    binstemp = 0:wid:(len); %+reps that was in there for reasons, but I'm not sure why
+    binstemp = 0:wid:(len); %KATE: +reps that was in there for reasons, but I'm not sure why
     bins(round(i/reps)+1,1:length(binstemp))=binstemp;
     val(round(i/reps)+1,1:length(binstemp)-1)=histcounts(data(:,i:i+reps-1),binstemp);   
 end
