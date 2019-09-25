362feb18966326a8ce2179a565a92026f669b85b
Angie Salles <angiesalles@gmail.com>
2019-09-25 16:58:18 -0400

    adapting for clutter stim

--- second_ana.m
+++ second_ana.m
@@ -1,5 +1,5 @@
 
-function [h1,h2,h3,pref_delay,pref_obj,pref_ang,means,use]=second_ana(spike_data,stim_data)
+function [h1,h2,h3,pref_delay,pref_obj,pref_clutter_distanc,means,use]=second_ana(spike_data,stim_data)
 %KA 2019
 %after prelim analysis, check neurons for responses and find some basics
 %like prefered delay and plots them
@@ -8,6 +8,7 @@ function [h1,h2,h3,pref_delay,pref_obj,pref_ang,means,use]=second_ana(spike_data
 %% initial sorting steps
 %check for <5 spikes per stim to throw out bad ones quick
 n=(max(spike_data.count)<=5);
+object_number=max(stim_data(:,2));
 
 if n~=1  %find peaks in post call hist
     pks_box=nan(size (spike_data.hist{1,2}));
@@ -19,12 +20,12 @@ if n~=1  %find peaks in post call hist
     end
     %tests if majority of responses have only one peak
     for q=1:size(locs_box,1)
-       test(q,1:2)=isnan(locs_box(q,1:2));
+        test(q,1:2)=isnan(locs_box(q,1:2));
     end
     test=sum(test,1);
     %creates a scatter plot of peaks from hist vals to check if timing is
     %consistent or not
-    if (test(1,2)-test(1,1))>3 %can be more or less strict. 2 works, but you gotta sift more. 
+    if (test(1,2)-test(1,1))>3 %can be more or less strict. 2 works, but you gotta sift more.
         disp('seems like theres only 1 peak, plotting to check timing')
         h=figure;
         for r=1:size(locs_box,1)
@@ -59,8 +60,9 @@ if n~=1  %find peaks in post call hist
     end
     %% starts generating plots if use==1
     if use==1 %find preferred stims based on total spike count (maybe try peak fr instead)
-        [pref_delay,pref_obj,pref_ang,means]=pref_finder(spike_data,stim_data);
+        [pref_delay,pref_obj,pref_clutter_distanc,means]=pref_finder(spike_data,stim_data);
         h1=figure;
+        
         set(h1,'Position',[150 150 1000 500])
         h(1)=subplot(1,3,1);
         hold on
@@ -68,18 +70,19 @@ if n~=1  %find peaks in post call hist
         scatter([5 10 15],means(1,1:3),'filled','k')
         xlim([0 20])
         title(h(1),'Delay')
+        xlabel('spike count(every dot is for the 20 presentationson that stim)')
         hold off
         h(2)=subplot(1,3,2);
         hold on
         scatter(stim_data(:,2),spike_data.count,'filled','b')
         scatter((1:4),means(2,1:4),'filled','k')
-        xlim([0 5])
+        xlim([0 (object_number+1)])
         title(h(2),'Object')
         hold off
         h(3)=subplot(1,3,3);
         hold on
         scatter(stim_data(:,3),spike_data.count,'filled','g')
-        xlim([-10 105])
+        xlim([-5 25])
         scatter([0 10 20],means(3,1:3),'filled','k')
         title(h(3),'Clutter distance')
         hold off
@@ -102,7 +105,7 @@ if n~=1  %find peaks in post call hist
         indLD=find(jitter_box(:,3)==3);
         indSD=find(jitter_box(:,3)==5);
         indMP=find(jitter_box(:,3)==6);
-        means(2,1,2)=mean(jitter_box(indcyl,1)); 
+        means(2,1,2)=mean(jitter_box(indcyl,1));
         means(2,2,2)=mean(jitter_box(indcube,1));
         means(2,3,2)=mean(jitter_box(indLD,1));
         means(2,4,2)=mean(jitter_box(indSD,1));
@@ -116,6 +119,7 @@ if n~=1  %find peaks in post call hist
         means(3,4,2)=nan;
         
         h2=figure;
+        
         set(h2,'Position',[150 150 1000 500])
         h(1)=subplot(1,3,1);
         hold on
@@ -123,19 +127,20 @@ if n~=1  %find peaks in post call hist
         scatter([5 10 15],means(1,1:3,2),'filled','k')
         xlim([0 20])
         title(h(1),'Delay')
+        xlabel('jitter(every dot is for the 20 presentationson that stim)')
         hold off
         h(2)=subplot(1,3,2);
         hold on
         scatter(stim_data(:,2),spike_data.jitter,'filled','b')
         scatter((1:4),means(2,1:4,2),'filled','k')
-        xlim([0 5])
+        xlim([0 (object_number+1)])
         title(h(2),'Object')
         hold off
         h(3)=subplot(1,3,3);
         hold on
         scatter(stim_data(:,3),spike_data.jitter,'filled','g')
-        scatter([0 45 90],means(3,1:3,2),'filled','k')
-        xlim([-10 105])
+        scatter([0 10 20],means(3,1:3,2),'filled','k')
+        xlim([-5 25])
         title(h(3),'Clutter distance')
         hold off
     end
@@ -150,9 +155,25 @@ if n~=1  %find peaks in post call hist
             elseif fr_box(i,3)==2
                 nom='cube';
             elseif fr_box(i,3)==3
-                nom='LD';
+                nom='shpere';
             elseif fr_box(i,3)==4
+                nom='LD';
+            elseif fr_box(i,3)==5
                 nom='SD';
+            elseif fr_box(i,3)==6
+                nom='MP';
+            elseif fr_box(i,3)==7
+                nom='AMPcyl';
+            elseif fr_box(i,3)==8
+                nom='AMPcube';
+            elseif fr_box(i,3)==9
+                nom='AMPshpere';
+            elseif fr_box(i,3)==10
+                nom='AMPLD';
+            elseif fr_box(i,3)==11
+                nom='AMPSD';
+            elseif fr_box(i,3)==12
+                nom='AMPMP';
             end
             x{i}=[num2str(fr_box(i,2)), num2str(fr_box(i,4)),nom];
         end
@@ -165,9 +186,9 @@ if n~=1  %find peaks in post call hist
         xtickangle(45)
         hold off
     end
-        
+    
 else
-    use=0;   
+    use=0;
 end
 if use==0
     h1=0;
@@ -175,8 +196,8 @@ if use==0
     h3=0;
     pref_delay=0;
     pref_obj=0;
-    pref_ang=0;
+    pref_clutter_distanc=0;
     means=0;
 end
-    % Kate: worth determinine peak fr from isi? probably unless most are very low
-    % firing
+% Kate: worth determinine peak fr from isi? probably unless most are very low
+% firing
