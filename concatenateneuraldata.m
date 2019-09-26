b401ad99417be5b204bfa2ab6099568d4cb7656e
Angie Salles <angiesalles@gmail.com>
2019-09-26 11:08:50 -0400

    scary scary bug in concatenate
    FT responses always comes before the stim respones, both for FT and 3D

--- concatenateneuraldata.m
+++ concatenateneuraldata.m
@@ -1,5 +1,5 @@
 
-day='20190910'
+day='20190628'
 
 path=['G:\Angie data\shapes project\',day,'\Matfile\'];
 
@@ -19,7 +19,7 @@ for idx2 = 1 : length(files)
         
 %  newpath=([path,strrep(files(idx2).name,'FT','3DplusFT'),filesep]);%this is for 3D
         newpath=([path,strrep(files(idx2).name,'FT','All'),filesep])% this is for clutter
-        mkdir(newpath)
-        save([newpath,channel,'.mat'],'data','sr')
+       % mkdir(newpath)
+       % save([newpath,channel,'.mat'],'data','sr')
     end
 end
