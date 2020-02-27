11b2e07ebd382863900e374a252c279ad77b9d01
kate-prime <kate.allen.89@gmail.com>
2020-02-27 17:06:28 -0500

    Update prelim_ana.m

--- prelim_ana.m
+++ prelim_ana.m
@@ -1,46 +1,30 @@
 %Modified from AS by KA 2019
-<<<<<<< HEAD
-function [fig,spike_data]=prelim_ana(fname,data,call_onset,delay,len,wind,reps,subProws)
-=======
-function [spike_data, fig]=prelim_ana(fname,data,call_onset,delay,len,reps,subProws,type)
->>>>>>> 7a55f90d7837dca246b385592c74044dc7ee2903
+%counts spikes and builds rasters for prelim data analysis
+function [spike_data, fig]=prelim_ana(fname,data,call_onset,delay,len,reps,subProws)
 
-%type: 1=clutter, 2=FT
 %FUTURE KATE: figure out if you wanna loop or run FT and clutter separate, just
 %does 3d for now
 %delay=length to stim onset-KATE: use 5 ms
 %len=length of stimulus in ms
 %wind=window of the response to analayze in ms
 %reps=stimulus repeats
-
+%type: 1=clutter, 2=FT
 %% load data and stim files
 spike_data.unit{1,1}=fname;%
-
-<<<<<<< HEAD
- 
-%Not strictly necessary, but handy
-matFile = ['/Volumes/Data1/Kate/KA001/KA001/stimuli/stims','.mat'];%loads stimuli
-if exist(matFile,'file')
-    load(matFile);
-    stim3D=stim2;
-    spike_data.stims{1,1}=stim3D;
-else
-    disp('you gonna need 3D stims')
-=======
-if type==1
-    %Not strictly necessary, but handy
-    matFile = ['D:\AngieDrive\Bats\NSF shapes project\neural_stim\clutterstim_org.mat'];%loads stimuli
-    if exist(matFile,'file')
-        load(matFile);
-        spike_data.stims{1,1}=stim_clutter;
-    else
-        disp('you gonna need stims')
-    end
-elseif type==2
-    stim_clutter=[];
-    spike_data.stims{1,1}=stim_clutter;
->>>>>>> 7a55f90d7837dca246b385592c74044dc7ee2903
-end
+%tidy this up
+% if type==1
+%     %Not strictly necessary, but handy
+%     matFile = ['D:\AngieDrive\Bats\NSF shapes project\neural_stim\clutterstim_org.mat'];%loads stimuli
+%     if exist(matFile,'file')
+%         load(matFile);
+%         spike_data.stims{1,1}=stim_clutter;
+%     else
+%         disp('you gonna need stims')
+%     end
+% elseif type==2
+%     stim_clutter=[];
+%     spike_data.stims{1,1}=stim_clutter;
+% end
 %
 %% make the bins
 [data,bins,val]=binfun(data,len,2,reps);
@@ -58,25 +42,16 @@ for i=1:(size(call_onset,1)/reps) %sorts stims
     ind=[ind,temp];
 end
 
-% FOR FT STIMS
-% ind1=(20:5:90);
-% for i=1:length(ind1)
-%     temp=find(call_onset(:,2)==ind1(i));
-%     ind=[ind,temp];
-% end
 call_onset=call_onset(ind,1);
 for n=1:size(call_onset,1)
     call_onset(n)=call_onset(n)+delay;
 end
 
-<<<<<<< HEAD
-%% Make the rasters 
-fig=makeras(data,len,reps,stim3D,subProws); %including stims here isn't necessesary, just adds a plot on the raster
-=======
+
 %% Make the rasters
 
 fig=makeras(data,len,reps,stim_clutter,subProws); %including stims here isn't necessesary, just adds a plot on the raster
->>>>>>> 7a55f90d7837dca246b385592c74044dc7ee2903
+
 
 %% set up storage cells
 num=(size(data,2)/reps);
@@ -89,6 +64,7 @@ spike_data.fr_all=NaN(num,1);
 spike_data.spike_times=data;
 
 %% generates a box of expected echo onset delays. only works if stims are in 5,10,15ms order
+%this may not be necessary anymore
 echos=[]; 
        for i=1:round(num/3) %makes a column corresponding to echo delays
            echos=[echos;[5;10;15]];
@@ -118,13 +94,12 @@ for x=1:(size(data,2)/reps)
     spike_data.count(x,:)=spk_number;
     spike_data.times(x,:)=times';
     spike_data.dur_total(x,:) = resp_dur_total;
-<<<<<<< HEAD
+
     spike_data.fr_all(x,:) = spikerate;
 
-=======
-    spike_data.fr(x,:) = spikerate;
+%    spike_data.fr(x,:) = spikerate;
     
->>>>>>> 7a55f90d7837dca246b385592c74044dc7ee2903
+
     %bad neurons will be filtered at next step by spike count and echo
     %response
 end
