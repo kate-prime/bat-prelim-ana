00e480f0e6570229bf67efacfb67b86589b6865a
angiesalles <angiesalles@gmail.com>
2020-09-11 18:14:25 -0400

    STIMSORT BIG BUG FIX for FT
    FT sorted now as frequs 20 to 90 steps of 5 at differnet dbs 20-70 in steps of 10, so first 15 are 20kHz at 20 dBs, columns 16 to 30 are 20khz at 30dBs, columns 31 to 45 are 20kHz at 40dBs etc

--- FTcode.m
+++ FTcode.m
@@ -1,4 +1,4 @@
-load('idbox.mat')
+load('cluster_id.mat')
 
 catUnits=idbox(find(cell2mat(idbox(:,1))==2),2);
 path=uigetdir; %now need to fish out from sorted?
@@ -12,5 +12,8 @@ for idx=1:1%size(catUnits,1)
     Chn=['C',depthChn{2}];
     WavClust=neuronInfo{3};
    
-    neuron2fish=([path,'\',date,'\Clut_',depth,'\',Chn,'_',WavClust,'_neruon.mat'])
+    neuron2fish=([path,'\',date,'\Clut_',depth,'\',Chn,'_',WavClust,'_neruon.mat']);
+    load(neruon2fish)
+    
+    
 end
