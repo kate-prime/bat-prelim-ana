71aed1ca8449120df9e3c53790a5daff1dcf6c9c
angiesalles <angiesalles@gmail.com>
2020-09-11 09:56:34 -0400

    progress on FT code

--- FTcode.m
+++ FTcode.m
@@ -1,8 +1,8 @@
 load('idbox.mat')
 
 catUnits=idbox(find(cell2mat(idbox(:,1))==2),2);
+path=uigetdir; %now need to fish out from sorted?
 
-%now need to fish out from uncat
 for idx=1:1%size(catUnits,1)
 
     neuronInfo=strsplit(catUnits{idx},'_');
@@ -11,5 +11,6 @@ for idx=1:1%size(catUnits,1)
     depth=depthChn{1};
     Chn=['C',depthChn{2}];
     WavClust=neuronInfo{3};
-    
+   
+    neuron2fish=([path,'\',date,'\Clut_',depth,'\',Chn,'_',WavClust,'_neruon.mat'])
 end
